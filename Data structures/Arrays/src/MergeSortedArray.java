337951a2fafb3a8b0d01560694f7a21ba979bb42
Khanh <chuhoangkhanh00@gmail.com>
2021-05-09 22:27:04 +0700

    test

---
+++ Data structures/Arrays/src/MergeSortedArray.java
@@ -0,0 +1,9 @@
+public class MergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int size = m + n;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {};
+    }
+}
